ccb4c606a310029d7fa3ab05460ac516a0214b5e
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-07-17 10:22:50 -0700

    Get started with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './frontend/**/*.js',
+    './**/*.liquid'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
