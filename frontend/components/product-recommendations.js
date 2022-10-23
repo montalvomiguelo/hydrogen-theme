2b2bcdeaabe001d133c1a1c0088cc607e9f49392
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-10-22 22:24:22 -0700

    Implement hydration directives
    borrowed from astro

--- frontend/components/product-recommendations.js
+++ frontend/components/product-recommendations.js
@@ -1,26 +1,19 @@
 class ProductRecommendations extends window.HTMLElement {
   connectedCallback () {
-    const handleIntersection = (entries, observer) => {
-      if (!entries[0].isIntersecting) return
-      observer.unobserve(this)
+    fetch(this.dataset.url)
+      .then(response => response.text())
+      .then(text => {
+        const html = document.createElement('div')
+        html.innerHTML = text
+        const recommendations = html.querySelector('product-recommendations')
 
-      fetch(this.dataset.url)
-        .then(response => response.text())
-        .then(text => {
-          const html = document.createElement('div')
-          html.innerHTML = text
-          const recommendations = html.querySelector('product-recommendations')
-
-          if (recommendations && recommendations.innerHTML.trim().length) {
-            this.innerHTML = recommendations.innerHTML
-          }
-        })
-        .catch(e => {
-          console.error(e)
-        })
-    }
-
-    new window.IntersectionObserver(handleIntersection.bind(this), { rootMargin: '0px 0px 400px 0px' }).observe(this)
+        if (recommendations && recommendations.innerHTML.trim().length) {
+          this.innerHTML = recommendations.innerHTML
+        }
+      })
+      .catch(e => {
+        console.error(e)
+      })
   }
 }
 
--- frontend/lib/revive.js
+++ frontend/lib/revive.js
@@ -1,8 +1,44 @@
+function media ({ query }) {
+  const mediaQuery = window.matchMedia(query)
+  return new Promise(function (resolve) {
+    if (mediaQuery.matches) {
+      resolve(true)
+    } else {
+      mediaQuery.addEventListener('change', resolve, { once: true })
+    }
+  })
+}
+
+function visible ({ element }) {
+  return new Promise(function (resolve) {
+    const observer = new window.IntersectionObserver(async function (entries) {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          observer.disconnect()
+          resolve(true)
+          break
+        }
+      }
+    })
+    observer.observe(element)
+  })
+}
+
+function iddle () {
+  return new Promise(function (resolve) {
+    if ('requestIdleCallback' in window) {
+      window.requestIdleCallback(resolve)
+    } else {
+      setTimeout(resolve, 200)
+    }
+  })
+}
+
 export default function revive (islands) {
   const knownJsSrcRE = /([^/\\]+)\.((j|t)sx?|m[jt]s|vue|marko|svelte|astro)($|\?)/
   const paths = Object.keys(islands)
 
-  function walk (node) {
+  async function walk (node) {
     const tagName = node.tagName
 
     const path = paths.find(path => {
@@ -11,16 +47,27 @@ export default function revive (islands) {
     })
 
     if (path) {
+      if (node.hasAttribute('client:visible')) {
+        await visible({ element: node })
+      }
+
+      const clientMedia = node.getAttribute('client:media')
+      if (clientMedia) {
+        await media({ query: clientMedia })
+      }
+
+      if (node.hasAttribute('client:iddle')) {
+        await iddle()
+      }
+
       islands[path]()
     }
 
-    let el = node.firstElementChild
+    const nextSibling = node.nextSibling
+    const firstChild = node.firstChild
 
-    while (el) {
-      walk(el)
-
-      el = el.nextElementSibling
-    }
+    if (nextSibling) walk(nextSibling)
+    if (firstChild) walk(firstChild)
   }
 
   walk(document.body)
