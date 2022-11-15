a478bd0aaf27a1939d74a18254347f63314db605
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-11-14 16:43:53 -0800

    Rename directory components to islands

--- frontend/entrypoints/theme.js
+++ frontend/entrypoints/theme.js
@@ -2,7 +2,7 @@ import 'vite/modulepreload-polyfill'
 import { initDetails } from '@/lib/a11y'
 import revive from '@/lib/revive.js'
 
-const islands = import.meta.glob('@/components/**.js')
+const islands = import.meta.glob('@/islands/**.js')
 const summaries = document.querySelectorAll('[id^="Details-"] summary')
 
 revive(islands)
