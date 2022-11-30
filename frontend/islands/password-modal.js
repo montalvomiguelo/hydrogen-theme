8f9126560f28d7761cc68033890d5a109d745a08
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-11-29 19:04:36 -0800

    Scripts for password page

---
+++ frontend/islands/password-modal.js
@@ -0,0 +1,11 @@
+import DetailsModal from './details-modal'
+
+class PasswordModal extends DetailsModal {
+  constructor () {
+    super()
+
+    if (this.querySelector('input[aria-invalid="true"]')) this.open({ target: this.querySelector('details') })
+  }
+}
+
+window.customElements.define('password-modal', PasswordModal)
