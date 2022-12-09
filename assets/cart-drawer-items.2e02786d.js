a31ff6b6686500d978276db2b36a9f7589d85205
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-12-09 14:01:45 -0800

    Build theme assets

---
+++ assets/cart-drawer-items.2e02786d.js
@@ -0,0 +1,1 @@
+import e from"./cart-items.9d5a809b.js";import"./utils.08d7e350.js";import"./theme.50788ea5.js";class t extends e{getSectionsToRender(){return[{id:"CartDrawer",section:"cart-drawer",selector:'[tabindex="-1"]'},{id:"cart-icon-bubble",section:"cart-icon-bubble",selector:".shopify-section"}]}}window.customElements.define("cart-drawer-items",t);
