3bddaf0054e510b5e8f2029950215ea3acdaec43
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-06-15 15:20:28 -0700

    Make Shopify deliver the original version of the assets

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import shopify from 'vite-plugin-shopify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [shopify()]
+  plugins: [shopify()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].[hash].min.js',
+        chunkFileNames: '[name].[hash].min.js',
+        assetFileNames: '[name].[hash].min[extname]'
+      }
+    }
+  }
 })
