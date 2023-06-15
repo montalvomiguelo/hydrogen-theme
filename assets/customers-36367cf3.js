d9b0a56a685dd3b531796557b973a5727798cebf
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-06-15 15:20:55 -0700

    Bundle theme assets

--- assets/customers-36367cf3.js
+++
@@ -1,1 +0,0 @@
-import{_}from"./modulepreload-polyfill-da3817bc.js";_(()=>import("./shopify_common-20499185.js"),[],import.meta.url);_(()=>import("./customer-address-254a8237.js"),[],import.meta.url);
