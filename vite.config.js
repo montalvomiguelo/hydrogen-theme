7932ffb169549564b4fb72c2578ae225e25f01e1
Miguel Montalvo <miguel.montalvo@meetdomaine.com>
2025-07-26 18:46:18 -0600

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, defaultAllowedOrigins } from 'vite'
 import shopify from 'vite-plugin-shopify'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [shopify(), tailwindcss()],
+  server: {
+    cors: {
+      origin: [defaultAllowedOrigins, /\.myshopify\.com$/]
+    }
+  },
   build: {
     rollupOptions: {
       output: {
