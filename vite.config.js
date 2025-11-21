0678418b48274af0bd5225deb21584b3c0af57d8
miguel <miguel.montalvo@meetdomaine.com>
2025-11-21 15:51:53 -0600

    Update vite and shopify vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig, defaultAllowedOrigins } from 'vite'
+import { defineConfig } from 'vite'
 import shopify from 'vite-plugin-shopify'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [shopify(), tailwindcss()],
-  server: {
-    cors: {
-      origin: [defaultAllowedOrigins, /\.myshopify\.com$/]
-    }
-  },
+  plugins: [shopify({ tunnel: true }), tailwindcss()],
   build: {
     rollupOptions: {
       output: {
