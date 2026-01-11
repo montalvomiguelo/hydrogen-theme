ae08e2d409ecc222fcc3c22303460fbb492b65fd
miguel <miguel.montalvo@meetdomaine.com>
2026-01-11 15:11:32 -0600

    Import maps are great

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
 import { defineConfig } from 'vite'
 import shopify from 'vite-plugin-shopify'
+import importMaps from 'vite-plugin-shopify-import-maps'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [shopify({ tunnel: true }), tailwindcss()],
+  plugins: [
+    shopify({ tunnel: true, versionNumbers: true }),
+    importMaps({
+      bareModules: {
+        defaultGroup: '@theme'
+      },
+      modulePreload: true
+    }),
+    tailwindcss()
+  ],
   build: {
+    minify: false,
     rollupOptions: {
       output: {
-        entryFileNames: '[name].[hash].min.js',
-        chunkFileNames: '[name].[hash].min.js',
-        assetFileNames: '[name].[hash].min[extname]'
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
       }
     }
   }
